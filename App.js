0bb8dcf24e9d883b2a4dfb3758cf862237ad2bc5
BorjaPardoJuanes <bopaju@alumnatflorida.es>
2025-09-25 08:20:27 +0200

    Código inicial copiado

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { View, Pressable, Text, Image, StyleSheet } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <Text style={styles.title}>My Title</Text>
+      <Image style={styles.image} source={require('./assets/snack-icon.png')} />
+      <Pressable>
+        <Text style={styles.text}>Púlsame!</Text>
+      </Pressable>
+    </View>);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const styles = StyleSheet.create({ 
+  container: { 
+    flex: 1, 
+    backgroundColor: 'yellow', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+  }, 
+  title: { 
+    fontSize: 25, 
+    fontWeight: 'bold', 
+    fontStyle: 'italic', 
+    textDecorationLine: 'underline', 
+  }, 
+  image: { 
+    width: 200, 
+    height: 200, 
+  }, 
+  text: { 
+    height: 40, 
+    width: 80, 
+    backgroundColor: 'blue', 
+    borderRadius: 8, 
+    padding: 6, 
+  }, 
 });
